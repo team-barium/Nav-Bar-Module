1f6247d74c8c7acf1e0c3a7bd5f8070e10427e63
Gabriella Pernama <gabypernama@gmail.com>
2019-02-17 09:57:20 -0800

    seated fake database

---
+++ database/data.js
@@ -0,0 +1,198 @@
+const categories = {
+  menAndWomen: [
+    {
+      featured: [
+        "New Arrivals",
+        "Best Sellers",
+        "Release Dates",
+        "YEEZY",
+        "Ultraboost",
+        "Harden Vol. 3",
+        "NMD",
+        "Statement Sneakers",
+        "Workout Essentials"
+      ],
+      shoes: [
+        "Originals",
+        "Running",
+        "Soccer",
+        "Basketball",
+        "Football",
+        "Skateboarding",
+        "Workout",
+        "Essentials",
+        "Sandals and Slides",
+        "Hiking",
+        "Golf",
+        "Tennis",
+        "Baseball",
+        "Sale Shoes"
+      ],
+      clothing: [
+        "Pants",
+        "Hoodies & Sweatshirts",
+        "Jackets & Vests",
+        "Track Suits",
+        "Short Sleeve Shirts",
+        "Graphic T-shirts",
+        "Long Sleeve Shirts",
+        "Jerseys",
+        "Tights",
+        "Shorts",
+        "Tank Tops",
+        "Sale Apparel"
+      ],
+      accessories: [
+        "Bags & Backpacks",
+        "Hats & Beanies",
+        "Socks",
+        "Phone Cases",
+        "Sunglasses",
+        "Balls",
+        "Watches",
+        "Gloves",
+        "All Accessories",
+        "Sale Accessories"
+      ],
+      sports: [
+        "Running",
+        "Soccer",
+        "Basketball",
+        "Football",
+        "Skateboarding",
+        "Training",
+        "Outdoor",
+        "Tennis",
+        "Golf",
+        "Hockey",
+        "Baseball",
+        "Lacrosse",
+        "Volleyball",
+        "Weightlifting",
+        "College Gear"
+      ]
+    }
+  ],
+  kids: [
+    {
+      featured: [
+        "New Arrivals",
+        "Best Sellers",
+        "Ultraboost",
+        "NMD",
+        "adicolor",
+        "Marquee Boost",
+        "Statement Sneakers"
+      ],
+      "Youth (8-14)": [
+        "Boys Shoes",
+        "Girls Shoes",
+        "Original Shoes",
+        "Sport Shoes",
+        "Boys Clothing",
+        "Girls Cothing",
+        "Hoodies & Sweatshirts",
+        "Pants & Shorts",
+        "Sale"
+      ],
+      "Children (4-8)": [
+        "Boys Shoes",
+        "Girls Shoes",
+        "Original Shoes",
+        "Sport Shoes",
+        "Boys Clothing",
+        "Girls Clothing",
+        "Sale"
+      ],
+      "Baby & Toddler (0-4)": ["Shoes", "Clothing", "Sale"],
+      Accessories: ["Socks", "Hats & Beanies", "Balls"]
+    }
+  ],
+  sports: [
+    {
+      running: [
+        "Shoes",
+        "Clothes",
+        "Accessories",
+        "Ultraboost",
+        "Alphabounce",
+        "Pureboost"
+      ],
+      soccer: [
+        "Shoes",
+        "Clothing",
+        "Accessories",
+        "Predator",
+        "X",
+        "Nemeziz",
+        "Copa"
+      ],
+      outdoor: [
+        "Shoes",
+        "Clothing",
+        "TERREX",
+        "Hiking Boots",
+        "Winter Jackets"
+      ],
+      basketball: [
+        "Shoes",
+        "Clothing",
+        "Accessories",
+        "James Harden",
+        "Marquee Boost",
+        "Pro Vision"
+      ]
+    }
+  ],
+  brands: [
+    {
+      originals: [
+        "Shoes",
+        "Clothing",
+        "Accessories",
+        "New Arrivals",
+        "YEEZY",
+        "P.O.D. System",
+        "NMB"
+      ],
+      "adidas athletics": [
+        "Men's clothing",
+        "Women's clothing",
+        "Kids Clothing",
+        "New Arrivals",
+        "Z.N.E.",
+        "ID"
+      ],
+      StellaMcartney: [
+        "Shoes",
+        "Clothing",
+        "Accessories",
+        "New Arrivals",
+        "Tennis",
+        "Training"
+      ],
+      addidasSkateboarding: [
+        "Shoes",
+        "Clothing",
+        "Accessories",
+        "New Arrivals",
+        "3MC",
+        "Matchcourt",
+        "Busenitz"
+      ],
+      collections: [
+        "Superstar",
+        "Stan Smith",
+        "EQT",
+        "Gazelle",
+        "Campus",
+        "Tubular",
+        "Samba",
+        "I-5923",
+        "adicolor Clothing"
+      ]
+    }
+  ]
+};
+
+module.exports = { categories };
